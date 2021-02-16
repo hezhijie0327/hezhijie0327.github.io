6f7051b232509ffbccea3e632c7d6ccc84be2561
Zhijie He <hezhijie0327@hotmail.com>
2021-02-16 13:59:41 +0800

    www.js | Version 1.0.1
    1. <Change> Change the description.
    2. <Change> Enhance performance.

--- assets/js/workers/www.js
+++ assets/js/workers/www.js
@@ -1,5 +1,5 @@
-// Current Version: 1.0.0
-// Description: Using Cloudflare Workers to publish hezhijie0327/hezhijie0327.github.io to www.zhijie.online.
+// Current Version: 1.0.1
+// Description: Using Cloudflare Workers to deploy hezhijie0327/hezhijie0327.github.io to www.zhijie.online.
 
 addEventListener("fetch", (event) => {
     event.respondWith(handleRequest(event.request));
@@ -9,9 +9,9 @@ async function handleRequest(request) {
     let url = request.url.substr(8);
     url = url.substr(url.indexOf("/") + 1);
     var response = await fetch("https://raw.githubusercontent.com/hezhijie0327/hezhijie0327.github.io/main/" + url);
-    if (url == "") {
+    if (url === "") {
         return Response.redirect("https://www.zhijie.online/index.html", 301);
-    } else if (response.status != 200) {
+    } else if (response.status !== 200) {
         var response = await fetch("https://raw.githubusercontent.com/hezhijie0327/hezhijie0327.github.io/main/404.html");
         return new Response(response.body, {
             status: 404,
