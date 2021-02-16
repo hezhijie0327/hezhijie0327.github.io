50fa8b1b342c81fa625a7edfad3c0febb8c82503
Zhijie He <hezhijie0327@hotmail.com>
2021-02-16 14:25:22 +0800

    source.js | Version 1.0.2
    1. <Change> Enhance performance.

--- assets/js/workers/source.js
+++ assets/js/workers/source.js
@@ -1,4 +1,4 @@
-// Current Version: 1.0.1
+// Current Version: 1.0.2
 // Description: Using Cloudflare Workers to map and mirror hezhijie0327's repos.
 
 addEventListener("fetch", (event) => {
@@ -20,10 +20,156 @@ async function handleRequest(request) {
         response = response_raw;
     }
     if (response !== "") {
-        return new Response(response.body, {
-            status: 200,
-            headers: response.headers,
-        });
+        if (url.includes(".conf") || url.includes(".dockerfile") || url.includes(".txt")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "text/plain;charset=UTF-8",
+                },
+            });
+        } else if (url.includes("css")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "text/css;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".dat")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "application/octet-stream;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".eot")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "application/vnd.ms-fontobject;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".gz")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "application/gzip;charset=UTF-8",
+                },
+            });
+        } else if (url.includes("html")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "text/html;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".jpg")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "image/jpeg;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".js")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "text/javascript;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".json")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "application/json;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".png")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "image/png;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".py")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "text/x-python;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".sh")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "application/x-sh;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".svg")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "image/svg+xml;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".ttf")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "font/ttf;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".woff")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "font/woff;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".woff2")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "font/woff2;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".yaml") || url.includes(".yml")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "application/yaml;charset=UTF-8",
+                },
+            });
+        } else if (url.includes(".zip")) {
+            return new Response(response.body, {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "content-type": "application/zip;charset=UTF-8",
+                },
+            });
+        } else {
+            return new Response(response.body, {
+                status: response.status,
+                headers: response.headers,
+            });
+        }
     } else {
         var response = await fetch("https://raw.githubusercontent.com/hezhijie0327/hezhijie0327.github.io/main/404.html");
         return new Response(response.body, {
