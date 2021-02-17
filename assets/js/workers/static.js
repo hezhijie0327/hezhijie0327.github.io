0c733f29af004d642328574e9b6864c497bd88ef
Zhijie He <hezhijie0327@hotmail.com>
2021-02-17 10:05:41 +0800

    static.js | Version 1.0.3
    1. <Change> Enhance performance.

--- assets/js/workers/static.js
+++ assets/js/workers/static.js
@@ -1,4 +1,4 @@
-// Current Version: 1.0.2
+// Current Version: 1.0.3
 // Description: Using Cloudflare Workers to deploy hezhijie0327/hezhijie0327.github.io's static files and redirect the files from jsDelivr.
 
 addEventListener("fetch", (event) => {
@@ -39,8 +39,10 @@ async function handleRequest(request) {
                     },
                 });
             } else if (url.includes(".html")) {
-                return new Response(response_hezhijie0327.body, {
-                    status: 200,
+                var response = await fetch("https://raw.githubusercontent.com/hezhijie0327/hezhijie0327.github.io/main/403.html");
+                response = await response.text();
+                return new Response(response.replace(/\<\!\-\-\ REASON\ \-\-\>/gim, '<script type="text/javascript">document.write(window.location.href);</script>'), {
+                    status: 403,
                     headers: {
                         "Access-Control-Allow-Origin": "*",
                         "content-type": "text/html;charset=UTF-8",
