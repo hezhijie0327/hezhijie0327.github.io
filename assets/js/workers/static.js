d87bce33e3045930e93df4352c16907947892849
Zhijie He <hezhijie0327@hotmail.com>
2021-02-19 16:33:45 +0800

    static.js | Version 1.0.7
    1. <Change> If the file size >= 512K, now the script will auto-redirect the file to "cdn.jsdelivr.net".

--- assets/js/workers/static.js
+++ assets/js/workers/static.js
@@ -1,4 +1,4 @@
-// Current Version: 1.0.6
+// Current Version: 1.0.7
 // Description: Using Cloudflare Workers to deploy hezhijie0327/hezhijie0327.github.io's static files and redirect the files from jsDelivr.
 
 addEventListener("fetch", (event) => {
@@ -29,99 +29,103 @@ async function handleRequest(request) {
         );
     } else {
         if (response_hezhijie0327.status === 200) {
-            if (url.includes(".css")) {
-                return new Response(response_hezhijie0327.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "text/css;charset=UTF-8",
-                    },
-                });
-            } else if (url.includes(".eot")) {
-                return new Response(response_hezhijie0327.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "application/vnd.ms-fontobject;charset=UTF-8",
-                    },
-                });
-            } else if (url.includes(".html")) {
-                var response = await fetch("https://raw.githubusercontent.com/hezhijie0327/hezhijie0327.github.io/main/status.html");
-                response = await response.text();
-                return new Response(
-                    response
-                        .replace(/\$\{COMMIT\}/gim, "哎呀！请在此处止步哟～")
-                        .replace(/\$\{DETAIL\}/gim, "您无权访问：" + '<script type="text/javascript">document.write(window.location.href);</script>')
-                        .replace(/\$\{STATUS\}/gim, "403"),
-                    {
-                        status: 403,
+            if (response_hezhijie0327.headers.get("Content-Length") <= 512 * 1024) {
+                if (url.includes(".css")) {
+                    return new Response(response_hezhijie0327.body, {
+                        status: 200,
                         headers: {
                             "Access-Control-Allow-Origin": "*",
-                            "content-type": "text/html;charset=UTF-8",
+                            "content-type": "text/css;charset=UTF-8",
                         },
-                    }
-                );
-            } else if (url.includes(".jpg")) {
-                return new Response(response_hezhijie0327.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "image/jpeg;charset=UTF-8",
-                    },
-                });
-            } else if (url.includes(".js")) {
-                return new Response(response_hezhijie0327.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "text/javascript;charset=UTF-8",
-                    },
-                });
-            } else if (url.includes(".png")) {
-                return new Response(response_hezhijie0327.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "image/png;charset=UTF-8",
-                    },
-                });
-            } else if (url.includes(".svg")) {
-                return new Response(response_hezhijie0327.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "image/svg+xml;charset=UTF-8",
-                    },
-                });
-            } else if (url.includes(".ttf")) {
-                return new Response(response_hezhijie0327.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "font/ttf;charset=UTF-8",
-                    },
-                });
-            } else if (url.includes(".woff")) {
-                return new Response(response_hezhijie0327.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "font/woff;charset=UTF-8",
-                    },
-                });
-            } else if (url.includes(".woff2")) {
-                return new Response(response_hezhijie0327.body, {
-                    status: 200,
-                    headers: {
-                        "Access-Control-Allow-Origin": "*",
-                        "content-type": "font/woff2;charset=UTF-8",
-                    },
-                });
+                    });
+                } else if (url.includes(".eot")) {
+                    return new Response(response_hezhijie0327.body, {
+                        status: 200,
+                        headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "content-type": "application/vnd.ms-fontobject;charset=UTF-8",
+                        },
+                    });
+                } else if (url.includes(".html")) {
+                    var response = await fetch("https://raw.githubusercontent.com/hezhijie0327/hezhijie0327.github.io/main/status.html");
+                    response = await response.text();
+                    return new Response(
+                        response
+                            .replace(/\$\{COMMIT\}/gim, "哎呀！请在此处止步哟～")
+                            .replace(/\$\{DETAIL\}/gim, "您无权访问：" + '<script type="text/javascript">document.write(window.location.href);</script>')
+                            .replace(/\$\{STATUS\}/gim, "403"),
+                        {
+                            status: 403,
+                            headers: {
+                                "Access-Control-Allow-Origin": "*",
+                                "content-type": "text/html;charset=UTF-8",
+                            },
+                        }
+                    );
+                } else if (url.includes(".jpg")) {
+                    return new Response(response_hezhijie0327.body, {
+                        status: 200,
+                        headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "content-type": "image/jpeg;charset=UTF-8",
+                        },
+                    });
+                } else if (url.includes(".js")) {
+                    return new Response(response_hezhijie0327.body, {
+                        status: 200,
+                        headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "content-type": "text/javascript;charset=UTF-8",
+                        },
+                    });
+                } else if (url.includes(".png")) {
+                    return new Response(response_hezhijie0327.body, {
+                        status: 200,
+                        headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "content-type": "image/png;charset=UTF-8",
+                        },
+                    });
+                } else if (url.includes(".svg")) {
+                    return new Response(response_hezhijie0327.body, {
+                        status: 200,
+                        headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "content-type": "image/svg+xml;charset=UTF-8",
+                        },
+                    });
+                } else if (url.includes(".ttf")) {
+                    return new Response(response_hezhijie0327.body, {
+                        status: 200,
+                        headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "content-type": "font/ttf;charset=UTF-8",
+                        },
+                    });
+                } else if (url.includes(".woff")) {
+                    return new Response(response_hezhijie0327.body, {
+                        status: 200,
+                        headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "content-type": "font/woff;charset=UTF-8",
+                        },
+                    });
+                } else if (url.includes(".woff2")) {
+                    return new Response(response_hezhijie0327.body, {
+                        status: 200,
+                        headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "content-type": "font/woff2;charset=UTF-8",
+                        },
+                    });
+                } else {
+                    return new Response(response_hezhijie0327.body, {
+                        status: response.status,
+                        headers: response.headers,
+                    });
+                }
             } else {
-                return new Response(response_hezhijie0327.body, {
-                    status: response.status,
-                    headers: response.headers,
-                });
+                return Response.redirect("https://cdn.jsdelivr.net/gh/hezhijie0327/hezhijie0327.github.io@main/" + url, 301);
             }
         } else if (response_jsdelivr_gh.status === 200) {
             return Response.redirect("https://cdn.jsdelivr.net/gh/" + url, 301);
